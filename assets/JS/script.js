a647ba4a8197a06c2ea1abeac138c355cab18a8d
JakeSchlei <jakeschlei@gmail.com>
2022-08-08 09:38:55 -0700

    Added all questions to the js file

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,7 +1,7 @@
 const questions = [
     {
         numb: 1,
-        quesiton: "Inside which HTML element do we put the JavaScript?",
+        question: "Inside which HTML element do we put the JavaScript?",
         answer: "<script>",
         options: [
             "<script>",
@@ -14,7 +14,7 @@ const questions = [
 
     {
         numb: 2,
-        quesiton: "How to write an IF statement in JavaScript?",
+        question: "How to write an IF statement in JavaScript?",
         answer: "if(i==5)",
         options: [
             "if i==5",
@@ -23,4 +23,40 @@ const questions = [
             "if i=5 then"
         ]
     },
-]
+
+    {
+        numb: 3,
+        question: "Arrays in JavaScript can be used to store what?",
+        answer: "All the Above",
+        options: [
+            "numbers and strings",
+            "booleans",
+            "other Arrays",
+            "All the Above"
+        ]
+    },
+
+    {
+        numb: 4,
+        question: "How do you call a function named myFunction?",
+        answer: "myFunction()",
+        options: [
+            "myFunction()",
+            "call myFunction",
+            "call = myFunction()",
+            "myFunction"
+        ]
+    },
+
+    {
+        numb: 5,
+        question: "How does a FOR loop start?",
+        answer: "for (i=0; i <= 5; i++)",
+        options: [
+            "for (i = 0; i < 5)",
+            "for i = 1 to 5",
+            "for (i = 0; i <= 5; i++)",
+            "for (i <= 5; i++)"
+        ]
+    },
+];
