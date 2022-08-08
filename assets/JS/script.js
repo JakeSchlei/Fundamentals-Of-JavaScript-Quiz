f14a74a0856b8824f69baff5aea13b9633fbba21
JakeSchlei <jakeschlei@gmail.com>
2022-08-08 09:13:14 -0700

    Initial Repo

---
+++ assets/JS/script.js
@@ -0,0 +1,26 @@
+const questions = [
+    {
+        numb: 1,
+        quesiton: "Inside which HTML element do we put the JavaScript?",
+        answer: "<script>",
+        options: [
+            "<script>",
+            "<scripting>",
+            "<javaScript",
+            "<js>"
+
+        ]
+    },
+
+    {
+        numb: 2,
+        quesiton: "How to write an IF statement in JavaScript?",
+        answer: "if(i==5)",
+        options: [
+            "if i==5",
+            "if i=5",
+            "if(i==5)",
+            "if i=5 then"
+        ]
+    },
+]
